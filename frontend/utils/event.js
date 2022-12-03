248a38cd82f29075e30571e22566a9f42d3e8d99
Sweta Kabi <swetakabi11@gmail.com>
2022-12-03 01:35:46 -0800

    rendering mustache

--- frontend/utils/event.js
+++ frontend/utils/event.js
@@ -1,11 +1,34 @@
+const $eventstream = document.getElementById("eventstream");
+const events = document.getElementById("events").innerHTML;
+
+
 const fetchEvents = async () => {
   apiClient.fetchAllEvents().then(async (httpRes) => {
       const status = httpRes.status;
       const res = await httpRes.json();
     if (status === 200) {
-          console.log(res);
-          return res;
+          console.log(res.events);
+          $eventstream.innerHTML = "";
+          res.events.forEach((event) => {
+
+            renderMessage(event);
+        });
       }
   });
 };
+const renderMessage = (message) => {
+
+const eventDisplay = {
+  title: message.title,
+  description: message.description,
+  end_time: message.end_time,
+      start_time: message.start_time,
+      date: message.date,
+      location: message.location
+  };
+console.log($eventstream)
+  const messageHtml = Mustache.render(events, eventDisplay);
+  $eventstream.insertAdjacentHTML("beforeend", messageHtml);
+};
+
 fetchEvents();
