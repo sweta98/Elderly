9f47c2b75bc365b9be1cb0f623b30a2c05592f8f
sweta98 <swetakabi11@gmail.com>
2022-12-02 23:24:04 -0800

    make files ejs and add partials (reusable header and footer)

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -7,20 +7,30 @@ const CWD = process.env.INIT_CWD
 */
 // home get example
 router.get('/', (req, res) => {
-    res.sendFile(CWD + '/frontend/views/index.html')
+   // res.sendFile(CWD + '/frontend/views/index.html')
+    res.render(CWD + '/frontend/views/index')
 })
 
 router.get('/createEvent', (req, res) => {
-    res.sendFile(CWD + '/frontend/views/createEvent.html')
+  //  res.sendFile(CWD + '/frontend/views/createEvent.html')
+    res.render(CWD + '/frontend/views/createEvent')
 })
 
 router.get('/events', (req, res) => {
-    res.sendFile(CWD + '/frontend/views/rsvpEvent.html')
+   // res.sendFile(CWD + '/frontend/views/rsvpEvent.html')
+    res.render(CWD + '/frontend/views/rsvpEvent')
 });
 
 router.get("/manageNeeds", (req, res) => {
-    res.sendFile(CWD + "/frontend/views/manageNeeds.html");
+  //  res.sendFile(CWD + "/frontend/views/manageNeeds.html");
+    res.render(CWD + '/frontend/views/manageNeeds')
 });
+
+router.get('/createEvent', (req, res) => {
+   // res.sendFile(CWD + '/frontend/views/createEvent.html')
+    res.render(CWD + '/frontend/views/createEvent')
+})
+
 
 /* 
     APIs
